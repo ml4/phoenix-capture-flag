6959f05598acdc69a42d2ef64e6eb5a7b291ec6a
ml4 <lewis@hashicorp.com>
2025-08-27 17:37:01 +0100

    Add templated workspace and project TFE provider repo template

---
+++ templates/terraform-tfe-project/outputs.tf
@@ -0,0 +1,4 @@
+output "tp-tp-main-id" {
+  value       = tfe_project.main.id
+  description = "TFE Project main ID"
+}
---
+++ templates/terraform-tfe-workspaces/outputs.tf
@@ -0,0 +1,4 @@
+output "tw-tw-main-id" {
+  value       = tfe_workspace.main.id
+  description = "TFE workspace main ID"
+}
